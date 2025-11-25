b2132086757aa949c9fce8daccc193baa736e09c
zvakilov29 <zvakilov2018@gmail.com>
2025-11-25 17:25:16 +0400

    Completed Lesson 02 Task 1: Salary Calculator

--- src/lesson2/Task1.java
+++ src/lesson2/Task1.java
@@ -2,6 +2,14 @@ package lesson2;
 
 public class Task1 {
     public static void main(String[] args) {
+        double grossSalary  = 2500;
+        double taxRate = 0.14;
 
+        double taxAmount = grossSalary * taxRate;
+        double netSalary = grossSalary - taxAmount;
+
+        System.out.printf("Gross Salary: %.2f AZN\n", grossSalary);
+        System.out.printf("Tax Amount: %.2f AZN\n", taxAmount);
+        System.out.printf("Net Salary: %.2f AZN\n", netSalary);
     }
 }
